cfee28f6b2e5b9ad8fcdab878644014e624a7b30
Ninos Ego <Ninos Ego@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-02 11:51:43 +0000

    Files updated for WP 3.6
    git-svn-id: https://plugins.svn.wordpress.org/flashcounter/tags/1.1.2@749937 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ flashcounter.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: FlashCounter
+Description: Includes the FlashCounter to your Page.
+Author: Ninos Ego
+Version: 1.1.2
+Author URI: http://ninosego.de/
+*/
+
+load_plugin_textdomain('flashcounter', false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+require_once('lib/admin.php');
+require_once('lib/footer.php');
